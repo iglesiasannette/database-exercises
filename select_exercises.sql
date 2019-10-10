27b7ffbc4075971d3b9c148e418defa2306018b1
Annette Iglesias <iglesiasannette@gmail.com>
2019-10-10 16:04:06 -0500

    select exercises complete

--- select_exercises.sql
+++ select_exercises.sql
@@ -3,3 +3,22 @@ USE codeup_test_db;
 SELECT name "These are the top selling Pink Floyd Albums: "
 FROM albums
 WHERE artist = 'Pink Floyd' ;
+
+SELECT release_date "The Beatles released Sgt. Pepper's Lonley Hearts Club in:  "
+FROM albums
+WHERE name = '%Sgt';
+
+SELECT genre "The album Nevermind is categorized in the this genre: "
+FROM albums
+WHERE name = 'Nevermind' ;
+
+
+SELECT name "The top selling albums released in the 90's were: "
+FROM albums
+WHERE release_date >= 1990
+AND release_date <=1999;
+
+SELECT name "The albums categorized in the rock genre are: "
+FROM albums
+WHERE genre = 'rock' ;
+
