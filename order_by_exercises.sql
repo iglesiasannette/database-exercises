7ac03003b2d84ab97122e2308a6efc01330773d7
Annette Iglesias <iglesiasannette@gmail.com>
2019-10-15 11:35:06 -0500

    exercise complete

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,62 +1,17 @@
-use employees;
+-- # Update your queries for employees whose names start and end with 'E'. Use concat()
+-- to combine their first and last name together as a single column in your results.
 
--- Create a new file called order_by_exercises.sql and copy and paste the contents
--- of your where_exercises.sql.
--- Modify your first query to order by first name. The first result should be
---  Irena Reutenauer and the last result should be Vidya Simmen.
-
-SELECT *
+SELECT CONCAT(first_name, ' ', last_name)
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name;
+WHERE last_name LIKE 'e%'
+AND last_name LIKE '%e';
 
--- Update the query to order by first name and then last name.
--- The first result should now be Irena Acton and the last should be Vidya Zweizig.
+-- For your query of employees born on Christmas and hired in the 90s, use datediff() to
+-- find how many days they have been working at the company (Hint: You will also need to
+-- use now() or curdate())
 
-SELECT *
+SELECT CONCAT(first_name, ' ', last_name) AS name, CONCAT('Working at the company for ', DATEDIFF(NOW(), hire_date) / 365, ' days') as years_at_company_message
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya')
-ORDER BY first_name, last_name;
-
--- Change the order by clause so that you order by last name before
--- first name. Your first result should still be Irena Acton but now the last
--- result should be Maya Zyda.
-
-SELECT *
-FROM employees
-WHERE first_name in ('Irena', 'Vidya', 'Maya')
-ORDER BY  last_name, first_name;
-
--- Update your queries for employees with 'E' in their last name
--- to sort the results by their employee number. Your results should not change!
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-ORDER BY emp_no;
-
-SELECT *
-FROM employees
-WHERE last_name LIKE 'E%'
-ORDER BY emp_no DESC ;
-
-
-select *
-from employees
-where last_name like 'E%'
-   or last_name like '%E' order by emp_no;
-
-
-select *
-from employees
-where last_name like 'E%E' order by emp_no desc;
-
--- Change the query for employees hired in the 90s and born on Christmas such
--- that the first result is the oldest employee who was hired last. It should be Khun Bernini.
-
-select *
-from employees
-where hire_date like '199%'
-and birth_date like '%12-25'
-order by birth_date desc,
-         hire_date asc;
+WHERE birth_date LIKE '%12-25'
+AND hire_date LIKE '199%'
+ORDER BY birth_date, hire_date DESC;
